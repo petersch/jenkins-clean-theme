18098c80b28ee8c9523bf494cf39e446737ac1a6
Peter Schmidt <petersch.home@gmail.com>
2014-08-01 18:20:37 +0200

    Fixed sidebar links using jQuery, removed some things.

--- master.js
+++ master.js
@@ -2,15 +2,21 @@
 var $jq = jQuery.noConflict(true);
 
 function stickyFoot() {
-  var bodyHeight = $jq("body.yui-skin-sam").height();
-  var vwptHeight = $jq(window).height();
-  var footHeight = 45;
-  var headerHeight = $jq("#header").height();
-  $jq("#main-table").css("min-height",vwptHeight-footHeight-headerHeight);
+	var bodyHeight = $jq("body.yui-skin-sam").height();
+	var vwptHeight = $jq(window).height();
+	var footHeight = 45;
+	var headerHeight = $jq("#header").height();
+	$jq("#main-table").css("min-height",vwptHeight-footHeight-headerHeight);
+}
+
+function sidePanelFix() {
+	$jq("a:contains('Previous Build')").prev().addClass("bg-arrow-left-icon");
+	$jq("a:contains('Next Build')").prev().addClass("bg-arrow-right-icon");
 }
 
 $jq(document).ready(function() {
   stickyFoot();
+  sidePanelFix();
 });
 
 $jq(window).resize(function() {
